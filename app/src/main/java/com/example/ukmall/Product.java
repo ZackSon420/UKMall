062f3921265d211370d8ded8bbdb13a5e1d763b4
SyahmiKhairul <syahmi2001@gmail.com>
2023-01-01 13:03:59 +0800

    Add product class (Tertinggal)

---
+++ app/src/main/java/com/example/ukmall/Product.java
@@ -0,0 +1,31 @@
+package com.example.ukmall;
+
+public class Product {
+    String name;
+    Integer price;
+
+    public Product(){
+    }
+
+    public Product(String name, Integer price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+}
+
