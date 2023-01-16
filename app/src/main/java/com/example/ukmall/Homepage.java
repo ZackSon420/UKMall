6f2810c1ac90e0cd2394519f51e72375a65919e9
adamadha8 <adamadhakz73@gmail.com>
2023-01-16 21:19:08 +0800

    Create User Session, Restrict User Access & Differentiate User Data in Firebase

--- app/src/main/java/com/example/ukmall/Homepage.java
+++ app/src/main/java/com/example/ukmall/Homepage.java
@@ -3,6 +3,7 @@ package com.example.ukmall;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -17,6 +18,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.ukmall.viewmodel.CartViewModel;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -43,9 +45,11 @@ public class Homepage extends AppCompatActivity implements View.OnClickListener{
     ArrayList<Product> productArrayList;
     FirebaseFirestore db;
     StorageReference storage;
+    private CartViewModel viewModel;
 
+    private SessionManager sessionManager;
     //button search
-    ImageView  iv_searchproduct;
+    ImageView  iv_searchproduct,iv_User;
 
     TextView tv_userName;
 
@@ -58,6 +62,12 @@ public class Homepage extends AppCompatActivity implements View.OnClickListener{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.homepage_activity);
+
+        viewModel = new ViewModelProvider(this).get(CartViewModel.class);
+
+
+        iv_User=findViewById(R.id.iv_user);
+        iv_User.setOnClickListener(this);
 
         tv_userName = findViewById(R.id.tv_userName);
         show_username();
@@ -93,6 +103,9 @@ public class Homepage extends AppCompatActivity implements View.OnClickListener{
         // Utk set Home as default
         bottomNavigationView.setSelectedItemId(R.id.home);
 
+        //Declare Session
+        sessionManager = new SessionManager(this);
+
 
         // Perform item selected listener untuk button BottomNavigationView
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -113,11 +126,12 @@ public class Homepage extends AppCompatActivity implements View.OnClickListener{
 
                     case R.id.addproduct:
                         startActivity(new Intent(getApplicationContext(), Add_Product.class));
-                        //Toast.makeText(Add_Product.class, "Add Product", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(Homepage.this, "Add Product", Toast.LENGTH_SHORT).show();
                         return true;
 
                     case R.id.account:
-                        Toast.makeText(Homepage.this, "User Account", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(getApplicationContext(),Analytics.class));
+                        Toast.makeText(Homepage.this, "Analytics", Toast.LENGTH_SHORT).show();
                         return true;
 
                 }
@@ -127,6 +141,7 @@ public class Homepage extends AppCompatActivity implements View.OnClickListener{
 
 
     }
+
 
     private void EventChangeListener(){
 
@@ -147,6 +162,7 @@ public class Homepage extends AppCompatActivity implements View.OnClickListener{
 
     }
 
+
     public void show_username(){
         Intent intent = getIntent();
 
@@ -158,11 +174,22 @@ public class Homepage extends AppCompatActivity implements View.OnClickListener{
     //Untuk function search
     @Override
     public void onClick(View view) {
+
+
         switch (view.getId()){
 
             case R.id.iv_searchprod:
                 Toast.makeText(this, "This is Search Function", Toast.LENGTH_LONG).show();
                 break;
+
+//Implements session for logout
+            case R.id.iv_user:
+                sessionManager.logout();
+                startActivity(new Intent(getApplicationContext(),Login.class));
+                Toast.makeText(Homepage.this, "Logout successfully!", Toast.LENGTH_SHORT).show();
+                viewModel.deleteAllCartItems();
+                break;
         }
     }
+
 }
