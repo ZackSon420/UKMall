6f872b5593ba08e60155b37869ec8dd3545c7a0a
adamadha8 <adamadhakz73@gmail.com>
2023-01-31 14:51:39 +0800

    Create Account User

--- app/src/main/java/com/example/ukmall/Homepage.java
+++ app/src/main/java/com/example/ukmall/Homepage.java
@@ -155,8 +155,10 @@ public class Homepage extends AppCompatActivity implements View.OnClickListener{
                         return true;
 
                     case R.id.account:
-                        startActivity(new Intent(getApplicationContext(),ManageOrder.class));
-                        Toast.makeText(Homepage.this, "ManageOrder", Toast.LENGTH_SHORT).show();
+                        //startActivity(new Intent(getApplicationContext(),ManageOrder.class));
+                        //Toast.makeText(Homepage.this, "ManageOrder", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(getApplicationContext(),Account_User.class));
+                        Toast.makeText(Homepage.this, "My Account", Toast.LENGTH_SHORT).show();
                         return true;
 
                 }
